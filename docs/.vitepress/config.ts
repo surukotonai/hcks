6c29c146ae327ac2e10b788d63ac0556a9db7a00
surukotonai <152766464+surukotonai@users.noreply.github.com>
2025-04-09 19:51:42 +0900

    test3

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/', 
+  base: '/wiki/', 
   title: 'My Docs',
   description: 'VitePress のサンプル',
   themeConfig: {
     nav: [
-      { text: 'Guide', link: '/guide/' },
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/wiki/' }
     ],
     sidebar: {
       '/guide/': [
         {
           text: 'ガイド',
           items: [
-            { text: '概要', link: '/' },
+            { text: '概要', link: '/guide/' },
             { text: '導入手順', link: '/guide/getting-started' }
           ]
         }
