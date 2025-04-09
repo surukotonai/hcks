d08f9cb5bfd4805aed941d1a59a097cd8a9ff0f5
surukotonai <152766464+surukotonai@users.noreply.github.com>
2025-04-09 22:41:25 +0900

    test3

--- wiki/.vitepress/config.ts
+++ wiki/.vitepress/config.ts
@@ -5,9 +5,8 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
 
   base: '/wiki/',
-  publicDir: '../assets',
   head: [
-    // ファビコンを指定。ファイルは publicDir で設定した assets からコピーされ、最終的に /wiki/favicon.ico に配置される
+    
     ['link', { rel: 'icon', type: 'image/x-icon', href: '/output_d747fef3701442e791ef05d65b48704b-uz0s6btr.ico' }]
   ],
   title: 'HackServer Docs',
