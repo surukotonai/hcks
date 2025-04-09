16df608ea0a28d66de613b1e275b2e8c1df5e53a
surukotonai <152766464+surukotonai@users.noreply.github.com>
2025-04-09 20:00:30 +0900

    ifg

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,21 +1,27 @@
+// docs/.vitepress/config.ts
+
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/wiki/', 
-  title:  'HackServer Docs',
-  description: 'test ',
+
+  base: '/wiki/',
+  title: 'HackServer Docs',
+  description: 'test',
   themeConfig: {
+
     nav: [
-      { text: 'Home', link: '/wiki/' },
-      { text: 'Guide', link: '/wiki/guide/' }
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' }
     ],
     sidebar: {
+
       '/guide/': [
         {
           text: 'ガイド',
           items: [
-            { text: '概要', link: '/wiki/guide/' },
-            { text: '導入手順', link: '/wiki/guide/getting-started' }
+
+            { text: '概要', link: '/guide/' },
+            { text: '導入手順', link: '/guide/getting-started' }
           ]
         }
       ]
